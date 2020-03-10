92f60241d0cbdcb1b6aa6fdb5b5d58357d82ab3d
nennogabriel <nennogabriel@gmail.com>
2020-03-10 20:27:24 -0300

    fixed chat

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -18,6 +18,7 @@ export const onRenderBody = (_ref, pluginOptions) => {
       src: "https://embed.tawk.to/" + tawkId + "/default",
       charset: "UTF-8",
       crossOrigin: "*",
+      SameSite: "None",
     }),
   ])
 }
